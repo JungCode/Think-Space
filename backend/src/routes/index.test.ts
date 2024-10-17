3f83967a4014513ac4abce8c7b81553294dcf989
Jung <buidinhtrung0704@gmail.com>
2024-10-17 09:29:01 +0700

    deploy be in CICD

--- backend/src/routes/index.test.ts
+++ backend/src/routes/index.test.ts
@@ -12,7 +12,6 @@ app.use('/', indexRoutes); // Gắn các route từ indexRoutes vào app
 describe('Test index route', () => {
   it('should return a JSON response with a message', async () => {
     const response = await request(app).get('/'); // Gửi yêu cầu GET tới '/'
-		console.log(response);
     expect(response.status).toBe(200); // Kiểm tra mã trạng thái HTTP
     expect(response.body).toEqual({ message: 'Hello this text comes from Back-end!' }); // Kiểm tra nội dung phản hồi
   });
