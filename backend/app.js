0f9fe2ba532e07c70b00504135a98bbb8dc2914f
Jung <buidinhtrung0704@gmail.com>
2024-10-17 08:22:22 +0700

    Prepare for deployment

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const index_1 = __importDefault(require("./routes/index"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+const port = process.env.PORT;
+app.use('/', index_1.default);
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ backend/controllers/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getIndex = void 0;
+const getIndex = (req, res) => {
+    res.json({ message: 'Hello this text comes from Back-end!' });
+};
+exports.getIndex = getIndex;
