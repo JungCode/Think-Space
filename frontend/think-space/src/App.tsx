f35b1a0693e8151a0f9ee6267fc89f98272e4c84
JungCode <buidinhtrung0704@gmail.com>
2024-10-19 21:28:02 +0700

    delete a few code for deploy

--- frontend/think-space/src/App.tsx
+++ frontend/think-space/src/App.tsx
@@ -1,8 +1,5 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import axios from 'axios';
-import Paragraph from './components/Paragraph';
 import { SignedIn, SignedOut } from '@clerk/clerk-react';
 import Landing from './pages/Landing';
 import DashBoard from './pages/DashBoard';
@@ -11,7 +8,6 @@ interface DataType {
   message: string; // Thay đổi theo cấu trúc thực tế của dữ liệu
 }
 function App() {
-  const [count, setCount] = useState(0);
   const [data, setData] = useState<DataType>();
   useEffect(() => {
     axios.get<DataType>('https://think-space-back-end-production.up.railway.app/')
