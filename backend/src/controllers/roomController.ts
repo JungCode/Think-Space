299e764d286963b2b49e62531e56f7b162f80603
Jung <buidinhtrung0704@gmail.com>
2024-12-08 15:11:20 +0700

    fix the argument at roomController

--- backend/src/controllers/roomController.ts
+++ backend/src/controllers/roomController.ts
@@ -19,7 +19,7 @@ export const saveARoom = async (req: Request, res: Response) => {
       documentId: req.body.documentId,
     };
     const userId = req.body.userId;
-    const roomRes = await Model.saveARoom(room, userId, roomId);
+    const roomRes = await Model.saveARoom(userId, roomId);
     res.status(201).send({ roomRes });
   } catch (error) {
     if (error instanceof Error) {
