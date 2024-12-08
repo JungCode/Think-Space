1bdef437054a1721deeabcd7deca3fb019caaf78
Jung <buidinhtrung0704@gmail.com>
2024-12-08 15:15:30 +0700

    run github action again

--- backend/src/config/liveblocks.ts
+++ backend/src/config/liveblocks.ts
@@ -1,6 +1,7 @@
 import { Liveblocks } from "@liveblocks/node";
 
 // Load environment variables
+// run git action again
 export const liveblocksConnection = () => {
   const liveblocks = new Liveblocks({
     secret: process.env.LIVEBLOCKS_SECRET_KEY || "",
