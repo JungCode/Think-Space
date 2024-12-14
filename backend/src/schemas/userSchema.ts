b6f9ed6e30fb0cbd97bad1e6e904f8bbcabcfb92
Jung <buidinhtrung0704@gmail.com>
2024-12-14 13:33:48 +0700

    fix test file

--- backend/src/schemas/userSchema.ts
+++ backend/src/schemas/userSchema.ts
@@ -4,11 +4,13 @@ import Joi from "joi";
 export const UserSchema = Joi.object({
   username: Joi.string().allow("").optional(),
   email: Joi.string().email().required(),
+  id: Joi.string().optional(),
   // role: Joi.string().optional(), // Add any other fields relevant to your User
 });
 
 export interface IUser {
   username?: string;
   email: string;
+  id?: string;
   // role?: string;
 }
